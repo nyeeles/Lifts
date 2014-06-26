88fdbad5787e653a8a785ad6046dd84dfe4a6984
Sasha Cooper <jinksy@gmail.com>
2014-06-26 21:41:17 +0100

    started to work in units of time

--- spec/LiftSpec.js
+++ spec/LiftSpec.js
@@ -97,13 +97,22 @@ describe("ElevatorSystem", function() {
   var system;
 
   beforeEach(function() {
-    system = new ElevatorSystem();
+    system = new ElevatorSystem(lift);
   })
 
   it("starts with 0 time elapsed", function() {
     expect(system.timeElapsed).toEqual(0);
   });
 
+  it("takes 1 time for an elevator to move up one floor", function(){
+    system.raiseLift(lift);
+    expect(system.timeElapsed).toEqual(1);
+  });
+
+  it("takes 1 time for an elevator to move down one floor", function(){
+    system.lowerLift(lift);
+    expect(system.timeElapsed).toEqual(1);
+  });
 
 });
 
