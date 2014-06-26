77c06c4735dc5a6f6d1cfc63da137ce7fa9b3299
Sasha Cooper <jinksy@gmail.com>
2014-06-26 21:25:49 +0100

    created elevator system class, extended elevators to have doors

--- spec/LiftSpec.js
+++ spec/LiftSpec.js
@@ -15,63 +15,118 @@
 //     expect(player).toBePlaying(song);
 //   });
 
+var lift;
+var floor2;
+
+beforeEach(function() {
+  lift = new Lift();
+});
+
 describe("Lift", function() {
 
-  var lift;
+  describe("going up and down", function() {
+
+    it("starts on the ground floor", function() {
+      expect(lift.floor).toEqual(0);
+    });
+
+    it("can go up to floor 4", function() {
+      expect(lift.topFloor).toEqual(4);
+
+    });
+
+    it("it's bottom floor is the basement", function() {
+      expect(lift.bottomFloor).toEqual(-1);
+    });
+
+    it("can go up", function() {
+      lift.goUp();
+      expect(lift.floor).toEqual(1);
+    });
+
+    it("can't go above floor 4", function() {
+      lift.floor = 4;
+      lift.goUp();
+      expect(lift.floor).toEqual(4);
+    });
+
+    it("can go down", function() {
+      lift.goDown();
+      expect(lift.floor).toEqual(-1);
+    });
+
+    it("can't go below the basement", function() {
+      lift.floor = -1;
+      lift.goDown();
+      expect(lift.floor).toEqual(-1);
+    });
+
+    it("can go to a specified floor froom any current floor", function() {
+      lift.goTo(4);
+      expect(lift.floor).toEqual(4);
+      lift.goTo(-1);
+      expect(lift.floor).toEqual(-1);
+    });
+
+
+  });
+
+  describe("opening and closing doors", function() {
+
+    it("starts with closed doors", function() {
+      expect(lift.hasClosedDoors).toBeTruthy();
+    });
+
+    it("can open its doors", function() {
+      lift.openDoors();
+      expect(lift.hasClosedDoors).toBeFalsy();
+    })
+
+    it("can close them again", function() {
+      lift.openDoors();
+      lift.closeDoors();
+      expect(lift.hasClosedDoors).toBeTruthy();
+    })
+
+  });
+
+});
+
+describe("ElevatorSystem", function() {
+  
+  var system;
+
+  beforeEach(function() {
+    system = new ElevatorSystem();
+  })
+
+  it("starts with 0 time elapsed", function() {
+    expect(system.timeElapsed).toEqual(0);
+  });
+
+
+});
+
+describe("Floor", function() {
 
   beforeEach(function() {
     lift = new Lift();
+    floor2 = new Floor(2, lift);
   });
 
-  it("starts on the ground floor", function() {
-    expect(lift.floor).toEqual(0)
+
+  it("calls a lift when an upButton is pressed", function() {
+    floor2.upButton();
+    expect(lift.floor).toEqual(2);
   });
 
-  it("can go up to floor 4", function() {
-    expect(lift.topFloor).toEqual(4)
-
+  it("calls a lift when a downButton is pressed", function() {
+    floor2.downButton();
+    expect(lift.floor).toEqual(2);
   });
 
-  it("it's bottom floor is the basement", function() {
-    expect(lift.bottomFloor).toEqual(-1)
-  });
-
-  it("can go up", function() {
-    lift.goUp()
-    expect(lift.floor).toEqual(1)
-  });
-
-  it("can't go above floor 4", function() {
-    lift.floor = 4
-    lift.goUp()
-    expect(lift.floor).toEqual(4)    
-  });
-
-  it("can go down", function() {
-    lift.goDown()
-    expect(lift.floor).toEqual(-1)
-  });
-
-  it("can't go below the basement", function() {
-    lift.floor = -1
-    lift.goDown()
-    expect(lift.floor).toEqual(-1)
-  });
-
-  it("can go to a specified floor froom any current floor", function() {
+  xit("opens the doors of a lift going up past it if upButton is pressed", function(){
     lift.goTo(4)
-    expect(lift.floor).toEqual(4)
-    lift.goTo(-1)
-    expect(lift.floor).toEqual(-1)
   })
-
-  it("goes to any floor where an up button was pressed", function() {
-    var floor2 = new Floor(2, lift)
-    expect(lift.floor).toEqual(0)
-    floor2.upButton()
-    expect(lift.floor).toEqual(2)
-  })
-
-  
 
 });
